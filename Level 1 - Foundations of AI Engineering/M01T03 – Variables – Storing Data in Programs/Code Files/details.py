588920667e3f4c6d15c430fd3d5fce012b3c8608
nathankap <thenathanplaza@gmail.com>
2026-03-17 12:44:16 -0400

    task 3

---
+++ Level 1 - Foundations of AI Engineering/M01T03 – Variables – Storing Data in Programs/Code Files/details.py
@@ -0,0 +1,6 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+house_number = input("What is your house number? ")
+street_name = input("What is your street name? ")  
+
+print(f"This is {name}. He is {age} years old and lives at house number {house_number} on {street_name}.")
