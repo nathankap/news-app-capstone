086ca4175660866a3fca1eca0008a37d803a2b54
nathankap <thenathanplaza@gmail.com>
2026-03-17 12:35:45 -0400

    hello world py

---
+++ Level 1 - Foundations of AI Engineering/M01T02 – Your First Computer Program/hello_world.py
@@ -0,0 +1,10 @@
+# ask user for name and save as variable "name"
+# print the variable name
+# ask user for age and save as variable "age"
+# print the variable age
+# print "Hello, World!"
+name = input("What is your name? ")
+print(name)
+age = input("What is your age? ")
+print(age)
+print("Hello, World!")
