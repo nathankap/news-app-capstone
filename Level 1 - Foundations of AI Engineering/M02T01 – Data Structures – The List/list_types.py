a51a46a89198546bbbe2e53ec5ee0e2eac50a084
nathankap <thenathanplaza@gmail.com>
2026-03-18 14:54:59 -0400

    2.1

---
+++ Level 1 - Foundations of AI Engineering/M02T01 – Data Structures – The List/list_types.py
@@ -0,0 +1,11 @@
+friends_names = ["Alice", "Bob", "Charlie"]
+print(friends_names[0]) # print alice
+print(friends_names[2]) # print charlie
+
+print(f"There are {len(friends_names)} friends in the list.")
+
+friends_ages = [25, 30, 35]
+
+print(f"{friends_names[0]} is {friends_ages[0]} years old.")
+print(f"{friends_names[1]} is {friends_ages[1]} years old.")
+print(f"{friends_names[2]} is {friends_ages[2]} years old.")
