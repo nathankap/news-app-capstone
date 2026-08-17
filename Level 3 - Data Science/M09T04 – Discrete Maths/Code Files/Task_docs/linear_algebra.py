f4ad185dc0ead1b98738403f2fc8342d6a10e48f
nathankap <thenathanplaza@gmail.com>
2026-08-17 18:06:28 -0400

    m9t4

--- Level 3 - Data Science/M09T04 – Discrete Maths/Code Files/Task_docs/linear_algebra.py
+++ Level 3 - Data Science/M09T04 – Discrete Maths/Code Files/Task_docs/linear_algebra.py
@@ -9,10 +9,19 @@ X = np.linspace(start_val, stop_val, n_samples)
 
 params = np.array([2, -5])
 
-"""
------------------
-Task 1
-------------------
+# Build the vector [x, 1] for each x value so we can compute P · [x, 1]
+X_with_bias = np.column_stack((X, np.ones_like(X)))
 
-Plot f(x) = P.X, where p is your params
-"""
+# y = P · [x, 1] = m*x + b
+f_x = X_with_bias @ params
+
+# Plot the line
+plt.plot(X, f_x, color='blue', label='f(x) = P · [x, 1]')
+plt.axhline(0, color='black', linewidth=0.8)
+plt.axvline(0, color='black', linewidth=0.8)
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.title('Linear Function from Parameter Vector')
+plt.grid(True)
+plt.legend()
+plt.show()
