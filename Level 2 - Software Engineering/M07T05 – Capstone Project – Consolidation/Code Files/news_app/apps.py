34da720c09abedafc5da135ab6fa36f747e00d16
nathankap <thenathanplaza@gmail.com>
2026-08-31 21:43:14 -0400

    docs: add docstrings to apps.py

--- Level 2 - Software Engineering/M07T05 – Capstone Project – Consolidation/Code Files/news_app/apps.py
+++ Level 2 - Software Engineering/M07T05 – Capstone Project – Consolidation/Code Files/news_app/apps.py
@@ -1,9 +1,14 @@
+"""App configuration module for news_app."""
+
 from django.apps import AppConfig
 
 
 class NewsAppConfig(AppConfig):
+    """Application configuration for the news_app Django application."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'news_app'
 
     def ready(self):
+        """Import signals when application registry is ready."""
         import news_app.signals  # noqa: F401
